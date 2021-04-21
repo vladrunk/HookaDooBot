db17b89f7c206db3a64118f8aebdfadb84b21cfb
Vladyslav Yurchenko <vld.yrchnk@gmail.com>
2021-04-22 02:55:29 +0300

    Reorganization of model registration in the admin panel

--- bot/admin.py
+++ bot/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
+from .models import *
 
-from django.apps import apps
 
-app_models = apps.get_app_config('bot').get_models()
-for model in app_models:
-    @admin.register(model)
-    class TobaccoOnSiteAdmin(admin.ModelAdmin):
-        list_display = [field.name for field in model._meta.fields]
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['uid', 'fname', 'lname', 'username', ]
+    search_fields = ['uid', 'fname', 'lname', 'username', ]
+
+
+@admin.register(Tobacco)
+class TobaccoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'weight', ]
+    list_filter = ['weight', ]
+    search_fields = ['title', ]
+
+
+@admin.register(TobaccoOnSite)
+class TobaccoOnSiteAdmin(admin.ModelAdmin):
+    list_display = ['site', 'title', 'title_on_site', ]
+    search_fields = ['site__title', 'title__title', 'title_on_site', ]
+
+
+@admin.register(Site)
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ['title', 'url', 'find_url', ]
+    search_fields = ['title', ]
+
+
+@admin.register(Search)
+class SearchAdmin(admin.ModelAdmin):
+    list_display = ['search_id', 'user', 'step', 'short_result', ]
+    list_filter = ['user', 'step', ]
+    search_fields = ['search_id', 'user__uid']
