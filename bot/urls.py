2089e1056ab17dad8a8dcc60d3319a97432e74ea
Vladyslav Yurchenko <vld.yrchnk@gmail.com>
2021-04-22 02:59:54 +0300

    Removing the parsing call from urls

--- bot/urls.py
+++ bot/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from .management.commands.parser import start
 from .services.cfg import BOT_TOKEN
 from .views import BotUpdate, robots_txt
 
 urlpatterns = [
-    path("robots.txt", robots_txt),
+    path(f'robots.txt', robots_txt),
     path(f'webhook/{BOT_TOKEN}', csrf_exempt(BotUpdate.as_view()), name='update'),
-    path(f'parser', start, name='parser'),
 ]
